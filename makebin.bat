9f2db498744a581a48f020113974fb82d68fc7d2
Sinan Salman(O) <sinan.salman@gmail.com>
2016-11-30 09:17:44 +0400

    removed unnecessary files

--- makebin.bat
+++
@@ -1,7 +0,0 @@
-@Echo off
-del docx2bb.pyc 
-del docx2bb.spec 
-rmdir BuildWIN
-rmdir __pycache__
-
-pyinstaller --workpath=.\BuildWIN --distpath=. --onefile .\docx2bb.py
--- makebin.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-rm -R docx2bb.pyc docx2bb.spec BuildOSX
-pyinstaller --workpath=./BuildOSX --distpath=. --onefile ./docx2bb.py
